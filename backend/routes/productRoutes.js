19b81acbc5e725bdef9933ee79926504203e9ffa
sanjaya1612 <sanjayahadipranata@gmail.com>
2021-11-16 11:53:44 +0700

    add redux

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,6 +5,7 @@ import Product from'../models/productModel.js'
 
 router.get('/', asyncHandler(async (req,res) => {
     const products = await Product.find({})
+    //throw new Error('Some error')
     res.json(products)
 }))
 router.get('/:id', asyncHandler (async(req,res) => {
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,51 +1,38 @@
-import React,{ useState, useEffect} from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
-import Rating from '../components/Rating'
-import { Link } from 'react-router-dom'
-import { Card } from 'react-bootstrap'
-import axios from 'axios'
+import Product from '../components/Product'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+import { listProducts } from '../actions/productActions'
 
 const HomeScreen = () => {
-    const [products, setProducts] = useState([])
+    const dispatch = useDispatch()
+
+    const productList = useSelector(state => state.productList)
+    const { loading, error, products } = productList
 
     useEffect(() => {
-        const fetchProducts = async () => {
-            const {data} = await axios.get('/api/products')
+        dispatch(listProducts())
+    }, [dispatch])
 
-            setProducts(data)
-        }
-
-        fetchProducts()
-    }, [])
     return (
         <>
             <h1>Favorite Activities</h1>
-            <br></br>
-            <Row>
-                {products.map(product => (
-                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                        <Card className='mb-3' style={{ width: '17rem' }}>
-                            <Link to={`/product/`+product._id}>
-                                <Card.Img variant="top" src={product.image} width= {110} height={200} />
-                            </Link>
-                            <Card.Body>
-                                <Link to={`/product/`+product._id}>
-                                    <Card.Title as='div'>
-                                        <strong>{product.name}</strong>
-                                    </Card.Title>
-                                </Link>
-                                <Card.Text as='div'>
-                                    <Rating
-                                        value={product.rating}
-                                        text={`${product.numReviews} reviews`}
-                                    />
-                                </Card.Text>
-                                <Card.Text as='h3'>Rp.{product.price}</Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                ))}
-            </Row>
+            
+            {loading ? (
+                <Loader />
+            ) : error ? (
+                <Message variant='danger'>{error}</Message>
+            ) : (
+                <Row>
+                    {products.map(product => (
+                        <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                            <Product product={product} />
+                        </Col>
+                    ))}
+                </Row>
+            )}
         </>
     )
 }
