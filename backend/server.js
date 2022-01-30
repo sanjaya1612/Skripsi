7819d3eadd3f0884f72a02c58e91130704ec72d6
sanjaya1612 <sanjayahadipranata@gmail.com>
2022-01-30 19:07:47 +0700

    .

--- backend/server.js
+++ backend/server.js
@@ -44,7 +44,7 @@ app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '/frontend/build')))
-    app.use(favicon(__dirname + '/public/favicon.ico'))
+    
 
     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend',
         'build', 'index.html'))
@@ -56,11 +56,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.use(notFound)
-
 app.use(errorHandler)
 
-// app.listen(process.env.PORT || 5000, function(){
-//     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-//   });
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
