2860e05bd0ea41f414faddd3656bffa79e39408d
sanjaya1612 <sanjayahadipranata@gmail.com>
2022-02-01 18:07:44 +0700

    filter admin

---
+++ frontend/src/components/BookingFilter.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { Form, Button, Row, Col } from 'react-bootstrap';
+
+const DateFoodFilter = ({ history }) => {
+    const [keydate, setKeydate] = useState('')
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        if (keydate) {
+            history.push(`/bookingfilter/${keydate}`)
+        } else {
+            history.push('/admin/foodorderlist')
+        }
+    }
+    return (
+        <>
+            <Form onSubmit={submitHandler}>
+                <Row>
+                    <Col>
+                        <Form.Control
+                            type='text'
+                            name='q'
+                            onChange={(e) => setKeydate(e.target.value)}
+                            placeholder='Search by username'
+                            className='datefilter'>
+                        </Form.Control>
+                    </Col>
+                    <Col>
+                        <Button type='submit' className='caritanggal'><i className="fas fa-search"></i></Button>
+                    </Col>
+                </Row>
+            </Form>
+        </>
+    )
+};
+
+export default DateFoodFilter;
